5d026c2a39a05c833ce2230e1c78a5c993def0c2
Julian Schuler <31921487+julianschuler@users.noreply.github.com>
2023-04-28 15:47:41 +0200

    Squashed commit of the following:
    commit ec9c0714f8d4c3d5dbd7167ab428158e1e57bbb8
    Author: Hendrik <git@h3ndrk.de>
    Date:   Fri Apr 28 08:30:00 2023 +0200
    
        --wip-- [skip ci]
    
    commit 882c3697e37fe98d302d88ded17990887244164f
    Author: Maximilian Schmidt <maximilian@schmidt.so>
    Date:   Fri Apr 28 15:33:00 2023 +0200
    
        variable extraction and renaming
    
    commit 6b8547ce081db31ebcaee0b39347cb9cc9b5c2e9
    Author: Maximilian Schmidt <maximilian@schmidt.so>
    Date:   Fri Apr 28 15:29:58 2023 +0200
    
        as mut and unwrap()
    
    commit 4f30579771195ae9c5996639dcac075f574c46d7
    Author: Maximilian Schmidt <maximilian@schmidt.so>
    Date:   Fri Apr 28 14:32:03 2023 +0200
    
        Fix the behavior simulator
    
    commit 747a03faca836d77d8fd56effc5d5df30fe8e190
    Author: Maximilian Schmidt <maximilian@schmidt.so>
    Date:   Fri Apr 28 14:26:36 2023 +0200
    
        Parameter tuning
    
    commit 258bf4f1853f344364472833a3cc4a718e395c00
    Author: Maximilian Schmidt <maximilian@schmidt.so>
    Date:   Fri Apr 28 12:26:14 2023 +0200
    
        is scoring goal
    
    commit 6686c35a7ad22c8d915d304c2060747f414cbe7c
    Author: Maximilian Schmidt <maximilian@schmidt.so>
    Date:   Fri Apr 28 11:04:50 2023 +0200
    
        Rabsolute
    
    commit cdef095c03370601933c9c7623a0c97a8175dac0
    Author: Maximilian Schmidt <maximilian@schmidt.so>
    Date:   Fri Apr 28 08:39:19 2023 +0200
    
        Fix signs
    
    commit ec82d1ed4eb6599c337b9044b47e140e5a4e2671
    Author: Maximilian Schmidt <maximilian@schmidt.so>
    Date:   Fri Apr 28 08:24:06 2023 +0200
    
        Introduce instant kick decisions
    
    commit 5751176a640ae9a14588a0c6279a207f610a8fc4
    Author: Maximilian Schmidt <maximilian@schmidt.so>
    Date:   Thu Apr 27 11:08:40 2023 +0200
    
        ball_state instead of ball_position
    
    commit 8175821e7e472182408604d184336847cf945616
    Author: Maximilian Schmidt <maximilian@schmidt.so>
    Date:   Wed Apr 26 15:46:32 2023 +0200
    
        Refactor kick decisions to a kick_selector

--- tools/twix/src/panels/map/layers/kick_decisions.rs
+++ tools/twix/src/panels/map/layers/kick_decisions.rs
@@ -13,8 +13,9 @@ use crate::{
 pub struct KickDecisions {
     robot_to_field: ValueBuffer,
     kick_decisions: ValueBuffer,
-    best_kick_decision: ValueBuffer,
+    instant_kick_decisions: ValueBuffer,
     kick_targets: ValueBuffer,
+    instant_kick_targets: ValueBuffer,
 }
 
 impl Layer for KickDecisions {
@@ -23,36 +24,55 @@ impl Layer for KickDecisions {
     fn new(nao: Arc<Nao>) -> Self {
         let robot_to_field =
             nao.subscribe_output(CyclerOutput::from_str("Control.main.robot_to_field").unwrap());
-        let kick_decisions = nao
-            .subscribe_output(CyclerOutput::from_str("Control.additional.kick_decisions").unwrap());
-        let best_kick_decision = nao.subscribe_output(
-            CyclerOutput::from_str("Control.additional.best_kick_decision").unwrap(),
+        let kick_decisions =
+            nao.subscribe_output(CyclerOutput::from_str("Control.main.kick_decisions").unwrap());
+        let instant_kick_decisions = nao.subscribe_output(
+            CyclerOutput::from_str("Control.main.instant_kick_decisions").unwrap(),
         );
         let kick_targets = nao
             .subscribe_output(CyclerOutput::from_str("Control.additional.kick_targets").unwrap());
+        let instant_kick_targets = nao.subscribe_output(
+            CyclerOutput::from_str("Control.additional.instant_kick_targets").unwrap(),
+        );
         Self {
             robot_to_field,
             kick_decisions,
-            best_kick_decision,
+            instant_kick_decisions,
             kick_targets,
+            instant_kick_targets,
         }
     }
 
     fn paint(&self, painter: &TwixPainter, _field_dimensions: &FieldDimensions) -> Result<()> {
         let robot_to_field: Isometry2<f32> = self.robot_to_field.require_latest()?;
         let kick_decisions: Vec<KickDecision> = self.kick_decisions.require_latest()?;
-        let best_kick_decision: Option<KickDecision> = self.best_kick_decision.require_latest()?;
+        let best_kick_decision = kick_decisions.first();
+        let instant_kick_decisions: Vec<KickDecision> =
+            self.instant_kick_decisions.require_latest()?;
         let kick_targets: Vec<Point2<f32>> = self.kick_targets.require_latest()?;
+        let instant_kick_targets: Vec<Point2<f32>> = self.instant_kick_targets.require_latest()?;
 
-        for kick_decision in kick_decisions {
+        for kick_decision in &kick_decisions {
             painter.pose(
-                robot_to_field * kick_decision.relative_kick_pose,
+                robot_to_field * kick_decision.kick_pose,
                 0.05,
                 0.1,
                 Color32::from_white_alpha(10),
                 Stroke {
                     width: 0.01,
                     color: Color32::BLACK,
+                },
+            );
+        }
+        for kick_decision in &instant_kick_decisions {
+            painter.pose(
+                robot_to_field * kick_decision.kick_pose,
+                0.05,
+                0.1,
+                Color32::from_white_alpha(10),
+                Stroke {
+                    width: 0.01,
+                    color: Color32::RED,
                 },
             );
         }
@@ -68,9 +88,20 @@ impl Layer for KickDecisions {
                 Color32::TRANSPARENT,
             )
         }
+        for kick_target in instant_kick_targets {
+            painter.target(
+                robot_to_field * kick_target,
+                0.1,
+                Stroke {
+                    width: 0.01,
+                    color: Color32::RED,
+                },
+                Color32::TRANSPARENT,
+            )
+        }
         if let Some(kick_decision) = best_kick_decision {
             painter.pose(
-                robot_to_field * kick_decision.relative_kick_pose,
+                robot_to_field * kick_decision.kick_pose,
                 0.05,
                 0.1,
                 Color32::from_white_alpha(10),
