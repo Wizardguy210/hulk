5764472d3723b10359b9589459b8988501d9674b
Vivituhh <vivienne.schwabe@tuhh.de>
2023-07-07 14:31:18 +0200

    move kick target position

--- crates/control/src/kick_selector.rs
+++ crates/control/src/kick_selector.rs
@@ -346,12 +346,12 @@ fn generate_goal_line_kick_targets(
 ) -> Vec<KickTarget> {
     let left_goal_half = field_to_robot
         * point![
-            field_dimensions.length / 2.0,
+            (field_dimensions.length / 2.0) + 0.05,
             field_dimensions.goal_inner_width / 4.0
         ];
     let right_goal_half = field_to_robot
         * point![
-            field_dimensions.length / 2.0,
+            (field_dimensions.length / 2.0) + 0.05,
             -field_dimensions.goal_inner_width / 4.0
         ];
     vec![
