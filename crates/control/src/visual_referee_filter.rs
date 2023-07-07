e4c6cee25f5a12d0e1346615aeec65c42a2ed873
Vivituhh <vivienne.schwabe@tuhh.de>
2023-07-07 14:13:58 +0200

    Squashed commit of the following:
    commit dce73baa425def2ffd782c352cc67104c0ec6d08
    Author: Patrick Göttsch <patrick.goettsch@tuhh.de>
    Date:   Wed Jul 5 23:34:19 2023 +0200
    
        Changes some timings
    
    commit 4b1dcfda1b9119b0cc1197e7a4ac40ec66a96897
    Author: Patrick Göttsch <patrick.goettsch@tuhh.de>
    Date:   Wed Jul 5 23:27:09 2023 +0200
    
        Removes unused variable, reset of time.
    
    commit 6692f3b3bf7cea513794bab8f459888cd99c54a5
    Author: Patrick Göttsch <patrick.goettsch@tuhh.de>
    Date:   Wed Jul 5 21:15:55 2023 +0200
    
        Adds secondary state
    
    commit bee288d92b334b94bad4d66243e147b4fe5c71d3
    Author: Patrick Göttsch <patrick.goettsch@tuhh.de>
    Date:   Tue Jul 4 17:10:36 2023 +0200
    
        Create StateTime

--- crates/control/src/visual_referee_filter.rs
+++ crates/control/src/visual_referee_filter.rs
@@ -1,16 +1,19 @@
 use num_traits::cast::FromPrimitive;
 use rand::prelude::*;
-use std::time::Duration;
+use std::time::{Duration, SystemTime};
 
 use color_eyre::{eyre::Context, Result};
 use context_attribute::context;
 use hardware::NetworkInterface;
-use spl_network_messages::VisualRefereeDecision;
 use spl_network_messages::{PlayerNumber, VisualRefereeMessage};
-use types::{messages::OutgoingMessage, CycleTime, FilteredWhistle, PrimaryState};
+use spl_network_messages::{SubState, VisualRefereeDecision};
+use types::{
+    messages::OutgoingMessage, CycleTime, FilteredWhistle, GameControllerState, PrimaryState,
+};
 
 pub struct VisualRefereeFilter {
     last_primary_state: PrimaryState,
+    time_of_last_visual_referee_related_state_change: Option<SystemTime>,
 }
 
 #[context]
@@ -19,6 +22,7 @@ pub struct CreationContext {}
 #[context]
 pub struct CycleContext {
     pub primary_state: Input<PrimaryState, "primary_state">,
+    pub game_controller_state: RequiredInput<Option<GameControllerState>, "game_controller_state?">,
     pub cycle_time: Input<CycleTime, "cycle_time">,
     pub filtered_whistle: Input<FilteredWhistle, "filtered_whistle">,
     pub player_number: Parameter<PlayerNumber, "player_number">,
@@ -34,25 +38,38 @@ impl VisualRefereeFilter {
     pub fn new(_context: CreationContext) -> Result<Self> {
         Ok(Self {
             last_primary_state: PrimaryState::Unstiff,
+            time_of_last_visual_referee_related_state_change: None,
         })
     }
 
     pub fn cycle(&mut self, context: CycleContext<impl NetworkInterface>) -> Result<MainOutputs> {
-        let send_visual_referee_message = matches!(
-            (self.last_primary_state, *context.primary_state),
+        match (self.last_primary_state, *context.primary_state) {
             (PrimaryState::Set, PrimaryState::Playing)
-                | (
-                    PrimaryState::Playing,
-                    PrimaryState::Finished | PrimaryState::Ready
-                )
-        );
+            | (PrimaryState::Playing, PrimaryState::Finished | PrimaryState::Ready)
+                if !matches!(
+                    context.game_controller_state.sub_state,
+                    Some(SubState::PenaltyKick)
+                ) =>
+            {
+                self.time_of_last_visual_referee_related_state_change =
+                    Some(context.cycle_time.start_time);
+            }
+            _ => {}
+        }
         self.last_primary_state = *context.primary_state;
 
-        // Initially a random visual referee decision
-        let mut rng = thread_rng();
-        let gesture = VisualRefereeDecision::from_u32(rng.gen_range(1..=13)).unwrap();
-
-        if send_visual_referee_message {
+        if self
+            .time_of_last_visual_referee_related_state_change
+            .map_or(false, |time_of_last_state_change| {
+                context
+                    .cycle_time
+                    .start_time
+                    .duration_since(time_of_last_state_change)
+                    .unwrap()
+                    .as_secs_f32()
+                    > 8.0
+            })
+        {
             let mut duration_since_last_whistle = context
                 .filtered_whistle
                 .last_detection
@@ -65,8 +82,13 @@ impl VisualRefereeFilter {
                 })
                 .unwrap_or(Duration::from_secs(15));
             if duration_since_last_whistle.as_secs_f32() < 1.0 {
-                duration_since_last_whistle = Duration::from_secs(5)
+                duration_since_last_whistle = Duration::from_secs(8)
             }
+
+            // Initially a random visual referee decision
+            let mut rng = thread_rng();
+            let gesture = VisualRefereeDecision::from_u32(rng.gen_range(1..=13)).unwrap();
+
             let message = OutgoingMessage::VisualReferee(VisualRefereeMessage {
                 player_number: *context.player_number,
                 gesture,
@@ -76,6 +98,8 @@ impl VisualRefereeFilter {
                 .hardware
                 .write_to_network(message)
                 .wrap_err("failed to write VisualRefereeMessage to hardware")?;
+
+            self.time_of_last_visual_referee_related_state_change = None;
         }
         Ok(MainOutputs::default())
     }
